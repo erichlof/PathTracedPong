5325187281396998b08561dd50cbb1698e3c4b80
Erich Loftis <erichlof@swbell.net>
2024-11-25 00:18:05 -0600

    Update Path_Traced_Pong_Fragment.glsl

--- shaders/Path_Traced_Pong_Fragment.glsl
+++ shaders/Path_Traced_Pong_Fragment.glsl
@@ -535,7 +535,7 @@ void main( void )
 	blueNoise = texelFetch(tBlueNoiseTexture, ivec2(mod(floor(gl_FragCoord.xy), 128.0)), 0).r;
 
 	vec2 pixelOffset = vec2( tentFilter(rand()), tentFilter(rand()) );
-	pixelOffset *= uCameraIsMoving ? 0.5 : 1.0;
+	pixelOffset *= 0.5;//uCameraIsMoving ? 0.5 : 1.0;
 
 	// we must map pixelPos into the range -1.0 to +1.0
 	vec2 pixelPos = ((gl_FragCoord.xy + vec2(0.5) + pixelOffset) / uResolution) * 2.0 - 1.0;
@@ -620,13 +620,9 @@ void main( void )
 	}
 
 	// makes sharp edges more stable
-	if (!uCameraIsMoving && previousPixel.a == 1.0)
-	{
-		if (pixelSharpness > 0.0)
-			currentPixel.a = 1.01;
-		else currentPixel.a = 1.0;
-	}
-		
+	if (previousPixel.a == 1.0)
+		currentPixel.a = 1.0;
+
 	// for dynamic scenes (to clear out old, dark, sharp pixel trails left behind from moving objects)
 	if (previousPixel.a == 1.0 && rng() < 0.05)
 		currentPixel.a = 0.0;
